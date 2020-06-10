5d1f15b238fdcd9bc62f4b8bee7a2bde157b0efd
Martin Schwier <martin@schwier.cc>
2020-06-10 15:35:59 +0200

    Update README.md and version

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # php-fpm-v8js
 
 Docker image based on the official php:7.4 with php [v8js](https://github.com/phpv8/v8js) extension.
-
-Doesn't compile currently due to v8js not yet supporting php 7.4.
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t maps82/php-fpm-v8js:latest .
-docker push maps82/php-fpm-v8js:latest
+docker build -t maps82/php-fpm-v8js:7.4.6-fpm-1 .
+docker push maps82/php-fpm-v8js:7.4.6-fpm-1
